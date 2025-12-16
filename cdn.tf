2b691e08a2004489d1a515d5fcd69542575eecce
Daniel Adedokun <adedokundaniel16@gmail.com>
2025-12-16 17:46:06 +0100

    Debugging resource creation"

--- cdn.tf
+++ cdn.tf
@@ -144,11 +144,6 @@ resource "aws_cloudfront_distribution" "website_cdn" {
   }
 }
 
-data "aws_route53_zone" "primary" {
-  name         = "lords.com."
-  private_zone = false
-}
-
 resource "aws_route53_record" "root_record" {
   zone_id = data.aws_route53_zone.primary.zone_id
   name    = var.domain_name  
