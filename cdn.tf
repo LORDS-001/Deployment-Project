1f8d4f842511949a56aa5f2ac6976eb4c582debd
Daniel Adedokun <adedokundaniel16@gmail.com>
2025-12-16 18:35:09 +0100

    Debugging resource creation"

--- cdn.tf
+++ cdn.tf
@@ -13,11 +13,13 @@ resource "aws_acm_certificate" "website_cert" {
   }
 }
 
-data "aws_route53_zone" "primary" {
-  name         = "lords.com."     
-  private_zone = false
-}
+resource "aws_route53_zone" "primary" {
+  name = "lords.com"
 
+  tags = {
+    Name = "lords.com Website"
+  }
+}
 resource "aws_route53_record" "cert_validation" {
   for_each = {
     for dvo in aws_acm_certificate.website_cert.domain_validation_options : dvo.domain_name => {
