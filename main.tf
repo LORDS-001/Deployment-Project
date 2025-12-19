4c61804dff74db9e85c7a2121efba7028c13bf34
Daniel Adedokun <adedokundaniel16@gmail.com>
2025-12-19 13:35:02 +0100

    Adding Lambda and DynamoDB

--- main.tf
+++ main.tf
@@ -43,3 +43,40 @@ resource "aws_s3_bucket_public_access_block" "public_access_block" {
 output "website_endpoint" {
   value = aws_s3_bucket_website_configuration.website_config.website_endpoint
 }
+
+resource "aws_dynamodb_table" "visitor_count" {
+  name         = "visitor-counter"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
+
+resource "aws_dynamodb_table_item" "init_count" {
+  table_name = aws_dynamodb_table.visitor_count.name
+  hash_key   = aws_dynamodb_table.visitor_count.hash_key
+  item       = <<ITEM
+{
+  "id": {"S": "counter"},
+  "count": {"N": "0"}
+}
+ITEM
+}
+
+data "archive_file" "zip_the_python_code" {
+  type        = "zip"
+  source_dir  = "${path.module}/lambda/"
+  output_path = "${path.module}/lambda/func.zip"
+}
+
+resource "aws_lambda_function" "my_func" {
+  filename      = data.archive_file.zip_the_python_code.output_path
+  source_code_hash = data.archive_file.zip_the_python_code.output_base64sha256
+  function_name = "visitor-counter-func"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "func.lambda_handler"
+  runtime       = "python3.9"
+}
