25a8a0062634b29564a4d119d5e6918a075cc21b
Daniel Adedokun <adedokundaniel16@gmail.com>
2025-12-19 13:41:46 +0100

    Adding Lambda and DynamoDB

--- main.tf
+++ main.tf
@@ -44,6 +44,26 @@ output "website_endpoint" {
   value = aws_s3_bucket_website_configuration.website_config.website_endpoint
 }
 
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
 resource "aws_dynamodb_table" "visitor_count" {
   name         = "visitor-counter"
   billing_mode = "PAY_PER_REQUEST"
